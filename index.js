357811c588c391d52d60888192444c4763367d63
[Tyler Coleman] <[himattthisistyler@gmail.com]>
2018-09-25 16:35:36 -0700

    added smooth scrolling to links with jquery

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+
+    $("#AboutMeLink").click(function() {
+        $(document.body).animate({
+            scrollTop: $("#About").offset().top
+        }, 700);
+    });
+    $("#ProjectsLink").click(function() {
+        $(document.body).animate({
+            scrollTop: $("#Projects").offset().top
+        }, 700);
+    });
+    $("#ResumeLink").click(function() {
+        $(document.body).animate({
+            scrollTop: $("#Resume").offset().top
+        }, 700);
+    });
+    $("#ContactLink").click(function() {
+        $(document.body).animate({
+            scrollTop: $("#Contact").offset().top
+        }, 700);
+    });
+});
