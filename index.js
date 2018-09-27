1ac5ec41b505e0c9b90318d993ea8764ef9f409b
[Tyler Coleman] <coleman.tyler@outlook.com>
2018-09-27 13:15:03 -0700

    added jquery-ui and fade in/out effects to some content

--- index.js
+++ index.js
@@ -20,4 +20,32 @@ $(document).ready(() => {
             scrollTop: $("#Contact").offset().top
         }, 700);
     });
+
+    $("#MusicianLabel").click(() => {
+        console.log('clicked label');
+        $("#MusicianContent").toggle("fade");
+        $("#MusicianPic").toggle("fade");
+    });
+    $("#CookLabel").click(() => {
+        console.log('clicked label');
+        $("#CookContent").toggle("fade");
+    });
+    $("#PilotLabel").click(() => {
+        console.log('clicked label');
+        $("#PilotContent").toggle("fade");
+        $("#PilotPic").toggle("fade");
+    });
+    $("#PedalLabel").click(() => {
+        console.log('clicked label');
+        $("#PedalContent").toggle("fade");
+        $("#PedalPic").toggle("fade");
+    });
+    $("#RateLabel").click(() => {
+        console.log('clicked label');
+        $("#RateContent").toggle("fade");
+    });
+    $("#BurgerLabel").click(() => {
+        console.log('clicked label');
+        $("#BurgerContent").toggle("fade");
+    });
 });
