111bb2f785a8313ef8c016528f058cf7f35c5981
[Tyler Coleman] <coleman.tyler@outlook.com>
2018-10-06 17:14:01 -0500

    added a little content to bio

--- index.js
+++ index.js
@@ -66,30 +66,24 @@ $(document).ready(() => {
     });
 
     $("#MusicianLabel").click(() => {
-        console.log('clicked label');
         $("#MusicianContent").toggle("fade");
         $("#MusicianPic").toggle("fade");
     });
     $("#CookLabel").click(() => {
-        console.log('clicked label');
         $("#CookContent").toggle("fade");
     });
     $("#PilotLabel").click(() => {
-        console.log('clicked label');
         $("#PilotContent").toggle("fade");
         $("#PilotPic").toggle("fade");
     });
     $("#PedalLabel").click(() => {
-        console.log('clicked label');
         $("#PedalContent").toggle("fade");
         $("#PedalPic").toggle("fade");
     });
     $("#RateLabel").click(() => {
-        console.log('clicked label');
         $("#RateContent").toggle("fade");
     });
     $("#BurgerLabel").click(() => {
-        console.log('clicked label');
         $("#BurgerContent").toggle("fade");
     });
 });
