93033af8417d0ea657f76a73371557141539f147
[Tyler Coleman] <coleman.tyler@outlook.com>
2018-10-16 15:39:08 -0700

    styling

--- index.js
+++ index.js
@@ -82,74 +82,21 @@ $(document).ready(() => {
     $(".AboutMeLink").click(() => {
 
         $(document.body).animate({
-            scrollTop: $("#" + activeSectionId).offset().top
+            scrollTop: $("#About").offset().top
         });
-
-        if (activeSectionId === 'About') {
-            $(document.body).animate({
-                scrollTop: $("#About").offset().top
-            });
-        } else {
-            $("#" + activeSectionId).hide("drop", 300, () => {
-                $("#About").show("fade", 300);
-            });
-            
-            activeSectionId = 'About';
-        }
     });
 
     $(".ProjectsLink").click(() => {
 
         $(document.body).animate({
-            scrollTop: $("#" + activeSectionId).offset().top
+            scrollTop: $("#Projects").offset().top
         });
-
-        if (activeSectionId === 'Projects') {
-            $(document.body).animate({
-                scrollTop: $("#Projects").offset().top
-            });
-        } else {
-            $("#" + activeSectionId).hide("drop", 300, () => {
-                $("#Projects").show("fade", 300);
-            });
-            
-            activeSectionId = 'Projects';
-        }
-    });
-
-    $(".ResumeLink").click(() => {
-
-        $(document.body).animate({
-            scrollTop: $("#" + activeSectionId).offset().top
-        });
-
-        if (activeSectionId === 'Resume') {
-            $(document.body).animate({
-                scrollTop: $("#Resume").offset().top
-            });
-        } else {
-            $("#" + activeSectionId).hide("drop", 300, () => {
-                $("#Resume").show("fade", 300);
-            });
-            
-            activeSectionId = 'Resume';
-        }
     });
 
     $(".ContactLink").click(() => {
         $(document.body).animate({
             scrollTop: $("#Contact").offset().top
         }, 700);
-    });
-
-    $("#MusicianLabel").click(() => {
-        $("#MusicianContent").toggle('fade');
-    });
-    $("#CookLabel").click(() => {
-        $("#CookContent").toggle('fade');
-    });
-    $("#PilotLabel").click(() => {
-        $("#PilotContent").toggle('fade');
     });
 
     $("#Toggle").click(() => {
